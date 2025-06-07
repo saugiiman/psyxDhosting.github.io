9da032fb84b6d4697bafc3ec9e895a5410636eb0
psyxD <saugatbry004@gmail.com>
2025-06-07 13:31:44 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,6 +1,5 @@
 // firebase-config.js
 
-// Your Firebase config (replace with yours)
 const firebaseConfig = {
   apiKey: "AIzaSyBpXFoOnbbVntQ7UEMSf08G_RCrBkLR1Fs",
   authDomain: "psyxdhosting.firebaseapp.com",
@@ -10,8 +9,8 @@ const firebaseConfig = {
   appId: "1:902116217148:web:ecb6e72830056019fe4e39"
 };
 
-// Initialize Firebase App
+// Initialize Firebase app
 firebase.initializeApp(firebaseConfig);
 
-// Initialize Firebase Auth **and assign to a global variable**
-const auth = firebase.auth();
+// Make auth global
+window.auth = firebase.auth();
