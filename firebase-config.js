1bd5dc32673b4ad140a591db3f0c03315c7acd81
psyxD <saugatbry004@gmail.com>
2025-06-07 13:15:06 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBpXFoOnbbVntQ7UEMSf08G_RCrBkLR1Fs",
+  authDomain: "psyxdhosting.firebaseapp.com",
+  projectId: "psyxdhosting",
+  storageBucket: "psyxdhosting.firebasestorage.app",
+  messagingSenderId: "902116217148",
+  appId: "1:902116217148:web:ecb6e72830056019fe4e39"
+};
+
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
 
