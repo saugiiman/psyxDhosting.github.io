f43d9467dc04b22317ed88a02961043a5cf91985
psyxD <saugatbry004@gmail.com>
2025-06-07 13:15:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+function login() {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  auth.signInWithEmailAndPassword(email, password)
+    .then(() => {
+      window.location.href = "dashboard.html";
+    })
+    .catch(e => alert(e.message));
+}
+
+function signup() {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  auth.createUserWithEmailAndPassword(email, password)
+    .then(() => {
+      window.location.href = "dashboard.html";
+    })
+    .catch(e => alert(e.message));
+}
 
